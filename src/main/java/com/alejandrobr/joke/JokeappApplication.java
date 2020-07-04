3645ba1e070e849a63c0552b0d8ce935fbd88890
alejandro <reyesbautista.contact@gmail.com>
2020-07-04 18:13:13 -0500

    Done, solved a problem in properties file.

--- src/main/java/com/alejandrobr/joke/JokeappApplication.java
+++ src/main/java/com/alejandrobr/joke/JokeappApplication.java
@@ -1,4 +1,4 @@
-package com.alejandrobr.joke.jokeapp;
+package com.alejandrobr.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/alejandrobr/joke/JokeappApplicationTests.java
+++ src/test/java/com/alejandrobr/joke/JokeappApplicationTests.java
@@ -1,4 +1,4 @@
-package com.alejandrobr.joke.jokeapp;
+package com.alejandrobr.joke;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
